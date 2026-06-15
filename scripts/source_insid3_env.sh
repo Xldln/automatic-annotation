332b78cfa7f67c474b7c2c2f0f767411034582b0
yy <yubinux@yeah.net>
2026-06-15 14:44:52 +0800

    add scripts

--- scripts/source_insid3_env.sh
+++ scripts/source_insid3_env.sh
@@ -14,4 +14,11 @@ cd CRF
 
 python setup.py install
 
-cd ..
+cd ../../../
+
+mkdir -p weights/insid3 && cd weights/insid3
+
+
+wget https://yubinux.cn/tmp/pt/dinov3_vitl16_pretrain_lvd1689m-8aa4cbdd.pth 
+
+cd ../../
